4985a5d68af4cc6f9edd705cd6c0f9bdb8a6276b
M SN <m.salehi1990@gmail.com>
2025-07-16 17:07:22 +0330

    add ChatMessage class

---
+++ ChatBotGPT/Models/ChatMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatBotGPT.Models
+{
+    public class ChatMessage
+    {
+        public string Role { get; set; } // "user" or "assistant"
+        public string Content { get; set; }
+    }
+
+}
