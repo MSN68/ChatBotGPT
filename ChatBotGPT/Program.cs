3b2b1176b255e4aa0128ef45aefc1c75f7494dc4
M SN <m.salehi1990@gmail.com>
2025-07-16 17:15:22 +0330

    config injective services

--- ChatBotGPT/Program.cs
+++ ChatBotGPT/Program.cs
@@ -1,10 +1,13 @@
 using ChatBotGPT.Components;
+using ChatBotGPT.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddHttpClient<OpenAIService>();
+builder.Services.AddSingleton<OpenAIService>();
 
 var app = builder.Build();
 
